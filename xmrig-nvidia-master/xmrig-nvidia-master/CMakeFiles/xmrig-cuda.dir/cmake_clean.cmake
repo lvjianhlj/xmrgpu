5024e10e570a132e3ceffcb1f269055d8a01cce0
lvjianhlj <178149184@qq.com>
2018-04-20 14:18:54 +0800

    12

---
+++ xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-cuda.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xmrig-cuda.dir/src/nvidia/xmrig-cuda_generated_cuda_core.cu.o"
+  "CMakeFiles/xmrig-cuda.dir/src/nvidia/xmrig-cuda_generated_cuda_extra.cu.o"
+  "libxmrig-cuda.pdb"
+  "libxmrig-cuda.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/xmrig-cuda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/DependInfo.cmake
@@ -0,0 +1,103 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/crypto/c_blake256.c" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/crypto/c_blake256.c.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/crypto/c_groestl.c" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/crypto/c_groestl.c.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/crypto/c_jh.c" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/crypto/c_jh.c.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/crypto/c_keccak.c" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/crypto/c_keccak.c.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/crypto/c_skein.c" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/crypto/c_skein.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAVE_SYSLOG_H"
+  "NDEBUG"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "XMRIG_NO_LIBCPUID"
+  "XMRIG_NO_NVML"
+  "XMRIG_NVIDIA_PROJECT"
+  "XMR_STAK_LARGEGRID=ON"
+  "_FORCE_INLINES"
+  "_GNU_SOURCE"
+  "_MWAITXINTRIN_H_INCLUDED"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  "src/3rdparty"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/App.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/App.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/App_unix.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/App_unix.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/Console.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/Console.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/Cpu_stub.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/Cpu_stub.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/Cpu_unix.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/Cpu_unix.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/Options.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/Options.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/Platform.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/Platform.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/Platform_unix.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/Platform_unix.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/Summary.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/Summary.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/api/Api.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/api/Api.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/api/ApiState.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/api/ApiState.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/api/Httpd.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/api/Httpd.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/api/NetworkState.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/api/NetworkState.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/crypto/CryptoNight.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/crypto/CryptoNight.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/log/ConsoleLog.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/log/ConsoleLog.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/log/FileLog.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/log/FileLog.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/log/Log.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/log/Log.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/log/SysLog.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/log/SysLog.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/net/Client.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/net/Client.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/net/Job.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/net/Job.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/net/Network.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/net/Network.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/net/SubmitResult.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/net/SubmitResult.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/net/Url.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/net/Url.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/net/strategies/DonateStrategy.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/net/strategies/DonateStrategy.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/net/strategies/FailoverStrategy.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/net/strategies/FailoverStrategy.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/net/strategies/SinglePoolStrategy.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/net/strategies/SinglePoolStrategy.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/nvidia/CudaCLI.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/nvidia/CudaCLI.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/nvidia/NvmlApi_stub.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/nvidia/NvmlApi_stub.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/workers/CudaWorker.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/workers/CudaWorker.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/workers/GpuThread.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/workers/GpuThread.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/workers/Handle.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/workers/Handle.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/workers/Hashrate.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/workers/Hashrate.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/workers/Workers.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/workers/Workers.cpp.o"
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/src/xmrig.cpp" "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-nvidia.dir/src/xmrig.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_SYSLOG_H"
+  "NDEBUG"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "XMRIG_NO_LIBCPUID"
+  "XMRIG_NO_NVML"
+  "XMRIG_NVIDIA_PROJECT"
+  "XMR_STAK_LARGEGRID=ON"
+  "_FORCE_INLINES"
+  "_GNU_SOURCE"
+  "_MWAITXINTRIN_H_INCLUDED"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  "src/3rdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/parallels/py/xmrig-nvidia-master/xmrig-nvidia-master/CMakeFiles/xmrig-cuda.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
